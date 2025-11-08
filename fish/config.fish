509ca73d59c6fd42324adad3e22522e5434fa4f6
Linuxury <161166960+Linuxury@users.noreply.github.com>
2025-11-08 18:49:21 -0500

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,6 @@
 if status is-interactive
+    # Set a writable path for fish variables
+    set -g fish_variables_path "$HOME/.local/share/fish/fish_variables"
     # Commands to run in interactive sessions can go here
     # Disable the welcome message
     set -g fish_greeting ""
